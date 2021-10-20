7b595282f0ff647c0f3f2f898d5108b37b31c83e
JRN03 <jonathan.nguyen2021@gmail.com>
2021-10-19 23:08:00 -0700

    login

--- testfile/testfile/ViewController.swift
+++ testfile/testfile/ViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import SCSDKLoginKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let loginButton = SCSDKLoginButton()
+        self.view.addSubview(loginButton)
     }
 
 
